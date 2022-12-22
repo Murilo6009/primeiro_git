1ee938720275c86b9464ab9034d2f462911af5be
murilo6009 <murilo_quagliato@hotmail.com>
2022-12-22 17:21:32 -0300

    codigo sofrido

---
+++ exemplo1.js
@@ -0,0 +1,48 @@
+// isso aqui é um comentario
+// vou te mostrar como exibir algo no terminal
+
+// console.log(typeof 'ola mundo');
+// console.log(typeof 1);
+// console.log(typeof 1.5);
+// console.log(typeof true);
+
+// let var1 = 1;
+
+// var var2 = 'isso é uma variavel';
+
+// function teste() {
+//     // console.log(var2);
+//     let var1 = 'abcde';
+
+//     const var3 = 25;
+
+//     console.log(var3 );
+
+// }
+
+// const var3 = 1;
+// console.log(var3);
+// teste();
+
+//Elaborar um algoritmo que calcule a área e o perímetro de um quadrado.
+
+
+let lado = 25
+
+console.log ( lado * 4  )
+
+
+//console.log (lado * 2)
+
+console.log (lado * lado)
+
+
+
+
+// console.log(var1 + var2 + var3);
+
+//console.log(1 + 1.5);
+
+//o javascript por padrão não necessita de ;
+//mas para se mater um padrão de codigo, ao final da dele se utiliza o ;, para ser amis facil de se enter e aplicar manutenções
+
