5eb1469551814640bf893062d0a22e447dceb0fc
murilo6009 <murilo_quagliato@hotmail.com>
2022-12-27 14:30:56 -0300

    exercicios 2 aprimorando cod

---
+++ Exercicio02/script.js
@@ -0,0 +1,9 @@
+
+// var nome = prompt('diga seu nome', 'Murilo');
+
+// var numero1 = prompt('digite o primeiro numero');
+// var numero2 = prompt('digite o segundo numero');
+
+// alert(numero1*numero2);
+// alert('Ola ' + nome);
+
